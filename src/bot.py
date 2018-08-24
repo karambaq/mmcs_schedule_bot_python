d81d3f6fb9c81f5fd79832dbad898b82fd511666
karambaq <0maximq@gmail.com>
2018-08-24 19:24:49 +0300

    Hide token

--- src/bot.py
+++ src/bot.py
@@ -3,14 +3,13 @@ import os
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
-TOKEN = "687954179:AAHiQy0nBRvkmrGA1Gk1rH8ZA7b3FsNcS68"
+TOKEN = os.environ.get('TOKEN')
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="wow")
 
 def polling():
     updater = Updater(token=TOKEN)
-    print('here')
     start_handler = CommandHandler('start', start) 
     updater.dispatcher.add_handler(start_handler) 
     updater.start_polling() 
